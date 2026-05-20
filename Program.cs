372a1819e0aef790ef13d1afb7e738923a00b80f
Michael Sterenberg <michast@microsoft.com>
2026-05-20 09:32:17 +0300

    Add sample application code

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.MapGet("/api/users/{id}", (int id) => new { Id = id, Name = "User" + id });
+
+app.Run();
